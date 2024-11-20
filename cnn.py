78bfb10970971da0935c4bfd0d7707c00907bbb1
Shrinidhi Krishnan <98567631+kshrin2002@users.noreply.github.com>
2024-11-20 17:20:26 -0500

    random

--- cnn.py
+++ cnn.py
@@ -7,7 +7,11 @@ from sklearn.metrics import classification_report
 
 class CNN():
     def __init__(self, x_train, x_test, y_train, y_test):
-        model = Sequential([
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.model = Sequential([
             # 1st Conv Layer
             Conv2D(32, (3, 3), padding="same", input_shape=(512, 512, 3)),
             BatchNormalization(),
@@ -34,12 +38,13 @@ class CNN():
             Dropout(0.5),  # Dropout to reduce overfitting
             Dense(1, activation="sigmoid")  # Binary classification
         ])
-        model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
-        self.history = model.fit(
+        self.model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+        self.history = self.model.fit(
             x_train, y_train,  # Training data
             batch_size=32,  # Number of samples per batch
             epochs=20,  # Number of training epochs
             verbose=1  # Display training progress
         )
-        self.test_loss, self.test_acc = model.evaluate(x_test, y_test)
-    
+        self.test_loss, self.test_acc = self.model.evaluate(x_test, y_test)
+        self.y_pred = (self.model.predict(x_test) > 0.5).astype("int32")
+
