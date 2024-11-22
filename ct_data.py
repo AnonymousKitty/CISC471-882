c9d49f6d3567840074b25c6566778b612800a939
AnonymousKitty <theonlycat2015@gmail.com>
2024-11-22 15:11:03 -0500

    attempts at reducing memory usage so we can use more data

--- ct_data.py
+++ ct_data.py
@@ -3,10 +3,9 @@ import pydicom
 import cv2
 
 class Ct_Data:
-    def __init__(self, res):
+    def __init__(self):
         self.dicoms = {}
         self.images = []
-        self.resolution = res
 
     def import_dicoms(self, new_item):
         item = pydicom.dcmread(new_item)
@@ -16,8 +15,7 @@ class Ct_Data:
     def load_images(self, dicom):
         pix_array = dicom.pixel_array
         img_normalized = cv2.normalize(pix_array, None, 0, 255, cv2.NORM_MINMAX)
-        img_resized = cv2.resize(img_normalized, self.resolution)
         # ct images should be rgb so we need to create 3 channels
-        img_rgb = cv2.merge([img_resized, img_resized, img_resized])
+        img_rgb = cv2.merge([img_normalized, img_normalized, img_normalized])
         self.images.append(img_rgb)
         return img_rgb
--- patient_data.py
+++ patient_data.py
@@ -1,17 +1,14 @@
 import ct_data
 import seg_data
 import os
-import cv2
-from copy import deepcopy
+# import cv2
+# from copy import deepcopy
 
 class Patient:
-    def __init__(self, path, resolution = (512, 512)):
-        # in case you want to modify the resolution, the option exists. myst be a tuple of 2 ints though
-        if len(resolution) != 2 or type(resolution[0]) != int or type(resolution[1]) != int:
-            raise IndexError("Resolution must be a tuple of 2 integer values")
+    def __init__(self, path):
         self.path = path
-        self.ct = ct_data.Ct_Data(resolution)
-        self.seg = seg_data.Seg_Data(resolution)
+        self.ct = ct_data.Ct_Data()
+        self.seg = seg_data.Seg_Data()
         self.load_data(self.path)
         # automatically assume this group is non cancerous
         self.labels = [0] * len(self.ct.images)
@@ -35,62 +32,64 @@ class Patient:
             elif os.path.isdir(path):
                 self.load_data(path)
 
-    # function that allows users to visualize data
-    def save_data_as_images(self, path):
-        ct_folder = path + "/ct"
-        seg_folder = path + "/segmentations"
-        # make sure there is a folder for the images if one does not already exist
-        if not os.path.exists(ct_folder):
-            os.makedirs(ct_folder)
-        if not os.path.exists(seg_folder):
-            os.makedirs(seg_folder)
-        # save images
-        for i, img in enumerate(self.ct.images):
-            cv2.imwrite(f"{ct_folder}/image_{i}.jpg", img)
-        for i, img in enumerate(self.seg.images):
-            cv2.imwrite(f"{seg_folder}/image_{i}.jpg", img)
+    # removing the following 2 functions to try to save memory
 
-    # function that allows users to see annotations on top of cts
-    def overlay_seg_on_ct(self, output_folder = None, color_options = [[255,0,0],[0,255,0],[0,0,255]]):
-        # local function to annotate images
-        def annotate(image, annotation, color):
-            img = image
-            for x, row in enumerate(annotation):
-                for y, col in enumerate(row):
-                    # if there is an annotation here, there will be a value > 0
-                    if col > 0:
-                        img[x,y] = color
-            return img
+    # # function that allows users to visualize data
+    # def save_data_as_images(self, path):
+    #     ct_folder = path + "/ct"
+    #     seg_folder = path + "/segmentations"
+    #     # make sure there is a folder for the images if one does not already exist
+    #     if not os.path.exists(ct_folder):
+    #         os.makedirs(ct_folder)
+    #     if not os.path.exists(seg_folder):
+    #         os.makedirs(seg_folder)
+    #     # save images
+    #     for i, img in enumerate(self.ct.images):
+    #         cv2.imwrite(f"{ct_folder}/image_{i}.jpg", img)
+    #     for i, img in enumerate(self.seg.images):
+    #         cv2.imwrite(f"{seg_folder}/image_{i}.jpg", img)
 
-        for i in color_options:
-            if len(i) != 3:
-                raise IndexError("color options must be a list of 3 values from 0 to 255")
-        overlay = []
-        # make a deep copy of the cts so we don't mess up the originals. value[1] will be the images
-        copied_dicoms = deepcopy(self.ct.dicoms)
-        # make sure the output folder exists
-        if output_folder:
-            path = output_folder + "/overlays"
-            if not os.path.exists(path):
-                os.makedirs(path)
-        # itterate through all the referenced uids 
-        for i, ref_id in enumerate(self.seg.dicoms.keys()):
-            # add the original image to the list of overlayed imaged
-            overlay.append(copied_dicoms[ref_id][1])
-            # itterate through each segmentation frame that references this uid and add the annotation to the image
-            for j, seg in enumerate(self.seg.dicoms[ref_id]):
-                color = color_options[j%len(color_options)]
-                img = overlay[i]
-                overlay[i] = annotate(img, seg, color)
-            # if an output path was provided, save the completed image to this path
-            if output_folder:
-                cv2.imwrite(f"{path}/image_{i}.jpg", overlay[i])
+    # # function that allows users to see annotations on top of cts
+    # def overlay_seg_on_ct(self, output_folder = None, color_options = [[255,0,0],[0,255,0],[0,0,255]]):
+    #     # local function to annotate images
+    #     def annotate(image, annotation, color):
+    #         img = image
+    #         for x, row in enumerate(annotation):
+    #             for y, col in enumerate(row):
+    #                 # if there is an annotation here, there will be a value > 0
+    #                 if col > 0:
+    #                     img[x,y] = color
+    #         return img
+
+    #     for i in color_options:
+    #         if len(i) != 3:
+    #             raise IndexError("color options must be a list of 3 values from 0 to 255")
+    #     overlay = []
+    #     # make a deep copy of the cts so we don't mess up the originals. value[1] will be the images
+    #     copied_dicoms = deepcopy(self.ct.dicoms)
+    #     # make sure the output folder exists
+    #     if output_folder:
+    #         path = output_folder + "/overlays"
+    #         if not os.path.exists(path):
+    #             os.makedirs(path)
+    #     # itterate through all the referenced uids 
+    #     for i, ref_id in enumerate(self.seg.dicoms.keys()):
+    #         # add the original image to the list of overlayed imaged
+    #         overlay.append(copied_dicoms[ref_id][1])
+    #         # itterate through each segmentation frame that references this uid and add the annotation to the image
+    #         for j, seg in enumerate(self.seg.dicoms[ref_id]):
+    #             color = color_options[j%len(color_options)]
+    #             img = overlay[i]
+    #             overlay[i] = annotate(img, seg, color)
+    #         # if an output path was provided, save the completed image to this path
+    #         if output_folder:
+    #             cv2.imwrite(f"{path}/image_{i}.jpg", overlay[i])
 
     def label_imgs(self):
         self.labels = []
         self.overall = 0
         for uid in self.ct.dicoms.keys():
-            if uid in self.seg.dicoms:
+            if uid in self.seg.ruids:
                 self.labels.append(1)
                 self.overall = 1
             else:
