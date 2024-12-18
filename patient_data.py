4f45dce3cd112b9299ff1fae13c168c9bfe7ea67
AnonymousKitty <theonlycat2015@gmail.com>
2024-12-17 22:42:36 -0500

    license update
    add license to all files that need it and remove .idea because it has useless files

--- patient_data.py
+++ patient_data.py
@@ -1,3 +1,19 @@
+#   patient_data file. this file contains code to store patient folder data, process ct and seg dicoms, and provide a label for each dicom
+#   Copyright (C) 2024 Catarina Borges McDiarmid, Mide Olanrewaju & Shrinidhi Thatahngudi Sampath Krishnan
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import pydicom
 
